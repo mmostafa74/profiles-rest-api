12b35673cb75de09c34a7fbc222209d749ee4cb8
Mohammad Mostafa <mmostafa@ieee-zsb.org>
2020-03-25 22:05:40 +0200

    Added viewset to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+
+router = DefaultRouter()    #config new router to the new viewset class
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') #generates a list of urls (functions) that assciated for viewset
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()), #first check the path from project file then search here if the url exists or not then load it to the browser
-
+    path('', include(router.urls))  #path in the list of urls and include right function
 ]
