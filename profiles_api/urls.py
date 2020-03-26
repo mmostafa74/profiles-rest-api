ab3add4bc8539dccdf664b6b4a920d6ed449d98e
Mohammad Mostafa <mmostafa@ieee-zsb.org>
2020-03-26 20:50:22 +0200

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,5 +10,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()), #first check the path from project file then search here if the url exists or not then load it to the browser
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))  #path in the list of urls and include right function
 ]
